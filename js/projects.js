eff84f010a44afb8bd8a4b69be8f399bd0c83d1a
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2026-06-06 16:39:46 -0300

    toggle sidebar funcional e outras alterações

--- js/projects.js
+++ js/projects.js
@@ -4,8 +4,13 @@ export function generateCards(){
 const container = document.querySelector('#projects')
 projects.forEach(({ name, description, imagepath, url }) => {
   const card = document.createElement('div')
-  card.className = 'flex flex-col bg-background text-foreground'
-  card.innerHTML = `<h3>${name}</h3><p>${description}</p><a href="${url}">View</a>`
+  card.className = 'flex flex-col bg-primary m-2 p-5 max-w-75 rounded-xl'
+  card.innerHTML = `<img src="${imagepath}"
+  class="rounded-xl"
+  /> 
+  <h3 class="font-mono font-bold" >${name}</h3>
+  <p>${description}</p>
+  <a href="${url}">View</a>`
   container.appendChild(card)
 })
 }
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,7 @@
 // Toggle sidebar visibility
+const sidebar = document.getElementById("sidebar");
+const button = document.getElementById("sidebar-btn");
+
+button.addEventListener("click", () => {
+  sidebar.classList.toggle("-translate-x-full");
+});
